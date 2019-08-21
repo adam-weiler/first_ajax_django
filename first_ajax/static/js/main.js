ff6f09fa74438082d5cdf7bbd4268db843d77417
adam-weiler <adam.weiler@yahoo.ca>
2019-08-21 16:34:54 -0400

    Adding Run Ajax Request button to index. Adding Event Listener to main.js, which makes request to herokuapp.

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  axios.get('http://intro-ajax-api.herokuapp.com/')
+    const ajaxRequestButton = document.querySelector('#get_root');
+    console.log('-- Logging out the button: ', ajaxRequestButton);
+
+    ajaxRequestButton.addEventListener('click', () => {
+        console.log('-- Clicked the Run AJAX Request Root button. ');
+
+        axios.get('http://intro-ajax-api.herokuapp.com/');
+
+    })
+
+    
 });
